ce68be0b2053427ea30f6f51baa19ceedc479a20
omkaragre <omkaragre5@gmail.com>
2024-01-31 18:26:59 +0530

    design responsive according to device.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <>
         <hr/>
-        <div className='bg-[#33B3A6] px-14 py-5 flex justify-between items-center'>
+        <div className='bg-[#33B3A6] px-7 py-2 md:px-14 md:py-5 sm:flex justify-between items-center gap-2'>
             <div>
                 <img src={WhiteLogo} alt="" width={200} />
                 <div className='flex items-center text-white'>
@@ -20,15 +20,13 @@ function Footer() {
                 </div>
             </div>
             <div>
-                <div>
-                    <a href="#" className='text-white underline-offset-2 px-5 hover:underline font-medium'>Products</a>
-                    <a href="#" className='text-white underline-offset-2 px-5 hover:underline font-medium'>About Us</a>
-                    <a href="#" className='text-white underline-offset-2 px-5 hover:underline font-medium'>Contact Us</a>
-                    <a href="#" className='text-white underline-offset-2 px-5 hover:underline font-medium'>Privacy Policy</a>
+                <div className='border border-indigo-700 grid sm:grid-flow-col auto-cols-auto sm:gap-4 md:gap-10'>
+                    <a href="#" className='text-white underline-offset-2  hover:underline font-medium'>Products</a>
+                    <a href="#" className='text-white underline-offset-2  hover:underline font-medium'>About Us</a>
+                    <a href="#" className='text-white underline-offset-2  hover:underline font-medium'>Contact Us</a>
+                    <a href="#" className='text-white underline-offset-2  hover:underline font-medium'>Privacy Policy</a>
                 </div>
             </div>
-            <p>tom
-            </p>
         </div>
     </>
   )
